97455d0c064ac2a1557e02cc48c1b6fa94acc548
Achal <106076516+slusy@users.noreply.github.com>
2024-12-26 23:18:02 +0530

    build: social links profile

--- CODE/NEWBIE/social-links-profile/vite.config.ts
+++ CODE/NEWBIE/social-links-profile/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://spudoodle.github.io/frontendmentor/out/social-links-profile",
+  build: {
+    outDir: "../../../out/social-links-profile",
+  },
+});
