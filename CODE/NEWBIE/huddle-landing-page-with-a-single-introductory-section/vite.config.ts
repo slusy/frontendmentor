e2aded1c98c3fe2e87470845796ab0c7d70368f0
spudoodle <achaldev3@gmail.com>
2025-01-15 14:56:27 +0530

    build: huddle landing page with a single introductory section

--- CODE/NEWBIE/huddle-landing-page-with-a-single-introductory-section/vite.config.ts
+++ CODE/NEWBIE/huddle-landing-page-with-a-single-introductory-section/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: "https://spudoodle.github.io/frontendmentor/out/huddle-landing-page-with-a-single-introductory-section",
+	build: {
+		outDir:
+			"../../../out/huddle-landing-page-with-a-single-introductory-section",
+	},
+});
